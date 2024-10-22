78d3172ea41d3691b8102c45d6d5c3840e77edb7
Golovin Maksim Dmitrievich <maks.golovin00@mail.ru>
2024-10-22 23:03:30 +0300

    Debugging models and endpoints
    Debug of endpoints for movies and usernames has been performed. Also new models were formed.

--- backend/app/api/routes/movies.py
+++ backend/app/api/routes/movies.py
@@ -1,79 +1,86 @@
 import csv
-import random
-from typing import List
 import os
-from fastapi import APIRouter, HTTPException, UploadFile, File
+import random
+from typing import List, Optional
+
+from fastapi import APIRouter, HTTPException, UploadFile, File, Depends
+from pydantic import ValidationError
 from sqlmodel import select
 
 from app.api.dependencies import SessionDepends
-from app.models import Movie, MovieCreate
-from typing import List, Optional
+from app.models import Movie, MovieCreate, MoviesFileUploadAnswer, MoviesPublic, DefaultAnswer
 
 router = APIRouter()
-# Directory to save uploaded images
-IMAGE_UPLOAD_DIR = "uploaded_images/"
 
-@router.post("/api/movies/upload_by_form/", response_model=Movie)
-def create_movie(session: SessionDepends, movie_data: MovieCreate):
-    movie = Movie.from_orm(movie_data)
+IMAGE_UPLOAD_DIR = "media_images/"
+MAX_FILE_SIZE = 2 * 1024 * 1024
+
+if not os.path.exists(IMAGE_UPLOAD_DIR):
+    os.makedirs(IMAGE_UPLOAD_DIR)
+
+@router.post(
+    "/api/movies/upload_by_form/",
+    description="Создаёт фильм/сериал с помощью формы. Если изображение отправлено и в файл, и в URL,"
+                " то берём изображение из файла",
+    response_model=Movie,
+)
+async def create_movie(
+    session: SessionDepends,
+    movie_data: MovieCreate = Depends(),
+    file: Optional[UploadFile] = File(None),
+):
+    movie = Movie.model_validate(movie_data)
+
+    if not movie.image_url and file is None:
+        raise HTTPException(status_code=400, detail="Invalid request. You have to choose at least one image.")
+
+    if file:
+        if file.content_type not in ["image/jpeg", "image/png"]:
+            raise HTTPException(status_code=400, detail="Invalid file type. Only JPEG and PNG are allowed.")
+
+        contents = await file.read()
+        if len(contents) > MAX_FILE_SIZE:
+            raise HTTPException(status_code=400, detail="File size exceeds the 2MB limit.")
+
+        file_path = os.path.join(IMAGE_UPLOAD_DIR, f'{movie.title}-{file.filename}')
+        movie.image_url = str(file_path)
+        with open(file_path, "wb") as f:
+            f.write(contents)
+
     session.add(movie)
     session.commit()
     session.refresh(movie)
     return movie
 
 
-@router.post("/api/movies/upload_by_form/", response_model=Movie)
-def create_movie(session: SessionDepends, movie_data: MovieCreate, image: UploadFile = File(None)):
-    image_url = None
-    if image:
-        file_extension = image.filename.split(".")[-1]
-        if file_extension not in ["jpg", "jpeg", "png", "img"]:
-            raise HTTPException(status_code=400, detail="Invalid image format. Only jpg, jpeg, png are allowed.")
-
-        image_filename = f"{movie_data.title}_{image.filename}"
-        image_path = os.path.join(IMAGE_UPLOAD_DIR, image_filename)
-
-        with open(image_path, "wb") as f:
-            f.write(image.file.read())
-        image_url = image_path
-
-    movie = Movie.from_orm(movie_data)
-    movie.image_url = image_url
-    session.add(movie)
-    session.commit()
-    session.refresh(movie)
-
-    return movie
-
-
-@router.post("/api/movies/upload_by_csv/")
-def upload_movies(session: SessionDepends, file: UploadFile = File(...)):
+@router.post(
+    "/api/movies/upload_by_csv/",
+    description='В файле необходимо указать такие поля как "title", "genre", "description", "year", "rating" и'
+                ' "image_url". Разделитель ";".',
+    response_model=MoviesFileUploadAnswer,
+)
+async def upload_movies(session: SessionDepends, file: UploadFile = File(...)):
     if file.content_type != "text/csv":
         raise HTTPException(status_code=400, detail="Invalid file type")
 
-    reader = csv.DictReader(file.file.read().decode("utf-8").splitlines())
-    movies_data = []
+    content = await file.read()
+    reader = csv.DictReader(content.decode("utf-8").splitlines(), delimiter=';')
 
+    movies_data: List[MovieCreate] = []
     for row in reader:
-        movie_data = {
-            "title": row["title"],
-            "genre": row["genre"],
-            "description": row.get("description"),
-            "year": int(row["year"]) if row.get("year") else None,
-            "rating": float(row["rating"]) if row.get("rating") else None,
-            "img_path": row.get["img_path"]
-        }
-        movies_data.append(movie_data)
+        try:
+            movies_data.append(
+                MovieCreate(**row)
+            )
+        except ValidationError as exc:
+            raise HTTPException(status_code=400, detail=f"Validation error: {exc}")
 
-    if not movies_data:
-        raise HTTPException(status_code=400, detail="CSV файл не содержит корректных данных о фильме")
     session.bulk_insert_mappings(Movie, movies_data)
     session.commit()
-    return {"message": "Фильмы успешно загружены"}
+    return MoviesFileUploadAnswer(message="Фильмы успешно загружены", content_amount = len(movies_data))
 
 
-
-@router.get("/api/movies/filter/", response_model=List[Movie])
+@router.get("/api/movies/", response_model=MoviesPublic)
 def get_movies(
         session: SessionDepends,
         genre: Optional[str] = None,
@@ -86,31 +93,31 @@ def get_movies(
     query = select(Movie)
 
     if genre:
-        query = query.where(Movie.genre == genre)
+        query = query.where(Movie.genre.icontains(genre))
     if title:
-        query = query.where(Movie.title.contains(title))
+        query = query.where(Movie.title.icontains(title))
 
     movies = session.exec(query.offset(offset).limit(page_size)).all()
-    return movies
+    return MoviesPublic(items=movies, count=len(movies))
 
 
-@router.delete("/api/movies/delete/")
-def delete_movie(session: SessionDepends, movie_id: int):
-    movie = session.get(Movie, movie_id)
+@router.delete("/api/movies/<content_id: int>/", response_model=DefaultAnswer)
+def delete_movie(session: SessionDepends, content_id: int):
+    movie = session.get(Movie, content_id)
     if not movie:
         raise HTTPException(status_code=404, detail="Фильм не найден")
     session.delete(movie)
     session.commit()
-    return {"message": "Фильм удален"}
+    return DefaultAnswer(message="Фильм удален")
 
 
-@router.get("/api/movies/random/", response_model=List[Movie])
+@router.get("/api/movies/random/", response_model=MoviesPublic)
 def get_random_movies(session: SessionDepends):
     movies = session.exec(select(Movie)).all()
     random_movies = random.sample(movies, min(10, len(movies)))
-    return random_movies
+    return MoviesPublic(items=random_movies, count=len(random_movies))
 
-@router.get("/api/movies/recommendations/", response_model=List[Movie])
+@router.get("/api/movies/recommendations/", response_model=MoviesPublic)
 def recommend_movies(
     genre: str,
     year: int,
@@ -121,8 +128,9 @@ def recommend_movies(
     max_year = year + 10
 
     query = select(Movie).where(
-        Movie.genre == genre,
+        Movie.genre.icontains(genre),
         min_year <= Movie.year,
         max_year >= Movie.year
     ).limit(limit)
-    return session.exec(query).all()
+    movies = session.exec(query).all()
+    return MoviesPublic(items=movies, count=len(movies))
--- backend/app/models.py
+++ backend/app/models.py
@@ -1,5 +1,13 @@
-from sqlmodel import Field, Relationship, SQLModel
+from email.policy import default
 from typing import Optional
+
+from sqlalchemy import Text
+from sqlmodel import Field, SQLModel
+
+
+class DefaultAnswer(SQLModel):
+    message: str
+
 
 class UserBase(SQLModel):
     username: str = Field(unique=True)
@@ -12,22 +20,31 @@ class User(UserBase, table=True):
 
 
 class UsersPublic(SQLModel):
-    data: list[UserBase]
+    items: list[UserBase]
     count: int
 
 
-class Movie(SQLModel, table=True):
-    id: int = Field(primary_key=True)
-    title: str
-    genre: str
-    description: Optional[str] = None
-    year: Optional[int] = None
-    rating: Optional[float] = None
-    image_url: Optional[str] = None
+class MovieBase(SQLModel):
+    title: str = Field(max_length=255)
+    genre: str = Field(max_length=255)
+    description: str = Field(sa_type=Text, nullable=False)
+    year: int
+    rating: float
+    image_url: Optional[str] = Field(max_length=255, nullable=True, default=None)
 
-class MovieCreate(SQLModel):
-    title: str
-    genre: str
-    description: Optional[str] = None
-    year: Optional[int] = None
-    rating: Optional[float] = None
+
+class MovieCreate(MovieBase):
+    pass
+
+
+class Movie(MovieBase, table=True):
+    id: Optional[int] = Field(default=None, primary_key=True)
+
+
+class MoviesPublic(SQLModel):
+    items: list[Movie]
+    count: int
+
+
+class MoviesFileUploadAnswer(DefaultAnswer):
+    content_amount: int
