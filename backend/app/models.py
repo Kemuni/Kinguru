5bc3405c7b5b72d726cf7fab3deaaa9b8111e2e8
Golovin Maksim Dmitrievich <maks.golovin00@mail.ru>
2024-12-15 02:03:27 +0300

    Обновление models.py
    Небольшой дебаг, моделька не видела другую

--- backend/app/models.py
+++ backend/app/models.py
@@ -77,42 +77,7 @@ class Movie(MovieBase, table=True):
     countries: list["Country"] = Relationship(back_populates="movies", link_model=CountryMovieLink)
 
 
-class MovieCreate(MovieBase):
-    genres: str = Field(description="Список названий жанров через запятую")
-    countries: str = Field(description="Список названий стран через запятую")
-    directors: str = Field(description="Список имен режиссеров через запятую")
-    ratings: list[RatingBase] = Field(description="Список рейтингов")
 
-    class Config:
-        json_schema_extra = {
-            "example": {
-                "imdb_id": "tt0111161",
-                "tmdb_id": 278,
-                "ru_title": "Побег из Шоушенка",
-                "en_title": "The Shawshank Redemption",
-                "original_title": "The Shawshank Redemption",
-                "description": "Банкир Энди Дюфрейн обвинён в убийстве собственной жены и её любовника.",
-                "image_path": "/9O7gLzmreU0nGkIB6K3BsJbzvNv.jpg",
-                "release_date": "1994-09-23",
-                "duration": 142,
-                "genres": "Драма, Криминал",
-                "countries": "США",
-                "directors": "Фрэнк Дарабонт",
-                "ratings": [
-                    {
-                        "source": "TMDB",
-                        "vote_average": 8.7,
-                        "vote_count": 24532
-                    }
-                ]
-            }
-        }
-
-    @field_validator('image_path')
-    def val_image_url(cls, value: str) -> Optional[str]:
-        assert value.startswith("/")
-        assert any(value.endswith(file_extension) for file_extension in [".png", ".jpg"])
-        return value
 
 
 class RatingBase(SQLModel):
@@ -152,6 +117,42 @@ ModelListToStr = Annotated[
     list[SQLModel], PlainSerializer(lambda items: ', '.join(item.name for item in items), return_type=str)
 ]
 
+class MovieCreate(MovieBase):
+    genres: str = Field(description="Список названий жанров через запятую")
+    countries: str = Field(description="Список названий стран через запятую")
+    directors: str = Field(description="Список имен режиссеров через запятую")
+    ratings: list[RatingBase] = Field(description="Список рейтингов")
+
+    class Config:
+        json_schema_extra = {
+            "example": {
+                "imdb_id": "tt0111161",
+                "tmdb_id": 278,
+                "ru_title": "Побег из Шоушенка",
+                "en_title": "The Shawshank Redemption",
+                "original_title": "The Shawshank Redemption",
+                "description": "Банкир Энди Дюфрейн обвинён в убийстве собственной жены и её любовника.",
+                "image_path": "/9O7gLzmreU0nGkIB6K3BsJbzvNv.jpg",
+                "release_date": "1994-09-23",
+                "duration": 142,
+                "genres": "Драма, Криминал",
+                "countries": "США",
+                "directors": "Фрэнк Дарабонт",
+                "ratings": [
+                    {
+                        "source": "TMDB",
+                        "vote_average": 8.7,
+                        "vote_count": 24532
+                    }
+                ]
+            }
+        }
+
+    @field_validator('image_path')
+    def val_image_url(cls, value: str) -> Optional[str]:
+        assert value.startswith("/")
+        assert any(value.endswith(file_extension) for file_extension in [".png", ".jpg"])
+        return value
 
 class MoviePublic(MovieBase):
     id: int
